0f718a011be03245f3544d60833fde1b64e63bc3
yamozha <yyamozha@gmail.com>
2019-03-04 14:15:34 +0200

    added caesar cypher

--- bot.py
+++ bot.py
@@ -13,18 +13,13 @@ from discord.ext import commands
 description = '''CryptoBot'''
 bot = commands.Bot(command_prefix='c!', description=description)
 
-async def status_task():
-    while True:
-        await bot.change_presence(game=discord.Game(name="c!help"))
-
-
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    bot.loop.create_task(status_task())
+    await bot.change_presence(game=discord.Game(name="c!help"))
 
 
 @bot.command()
@@ -43,6 +38,33 @@ async def encode_binary(st):
    """ Encode Text to Binary Code """
    st = str(st)
    await bot.say(' '.join(format(ord(x), 'b') for x in st))
+
+
+@bot.command()
+async def caesar(text):
+    """ encodes clear text to the caesar cipher """
+    L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
+    I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+    key = 3
+    plaintext = str(plaintext)
+
+    # encipher
+    ciphertext = ""
+    for c in plaintext.upper():
+        if c.isalpha(): ciphertext += I2L[ (L2I[c] + key)%26 ]
+        else: ciphertext += c
+
+        # decipher
+        plaintext2 = ""
+        for c in ciphertext.upper():
+            if c.isalpha(): plaintext2 += I2L[ (L2I[c] - key)%26 ]
+            else: plaintext2 += c
+
+            await bot.say(plaintext)
+            await bot.say(ciphertext)
+            await bot.say(plaintext2)
+
 
 
 bot.run(TOKEN)
